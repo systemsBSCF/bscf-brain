41d090e5efc3c0f7ace606ad8eacfc8479df2bca
systemsBSCF <155564165+systemsBSCF@users.noreply.github.com>
2024-04-08 18:43:33 +0300

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -3,7 +3,7 @@ import requests
 def fetch_data_from_backend(prospect_id, sales_rep_id):
     # Replace the URL below with the actual URL of your Flask backend
     api_url = "https://obainetwork.replit.app/fetch_user_data"
-    params = {"?user_id": prospect_id, "module_api_name": 'Accounts'}
+    params = {"user_id": prospect_id, "module_api_name": 'Accounts'}
     response = requests.get(api_url, params=params)
     if response.status_code == 200:
         return response.json()  # Assuming your Flask API returns JSON
