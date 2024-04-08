bc72f778445cfa1f521a0c613c80eeaba65f63a7
systemsBSCF <155564165+systemsBSCF@users.noreply.github.com>
2024-04-08 20:59:19 +0300

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -2,7 +2,7 @@ import requests
 
 def fetch_data_from_backend(prospect_id):
     # Replace the URL below with the actual URL of your Flask backend
-    api_url = "https://wideeyed-forked-enterprise-benatarofir.replit.app/"
+    api_url = "https://wideeyed-forked-enterprise-benatarofir.replit.app/fetch_user_data"
     params = {"user_id": prospect_id, "module_api_name": 'Accounts'}
     response = requests.get(api_url, params=params)
     if response.status_code == 200:
